9862c34637d0a8a8ad0f69318e75fdadf8ad4f48
Michael Cordingley (mcordingley) <Michael.Cordingley@gmail.com>
2015-02-28 18:30:57 -0500

    Move linear least squares tests out into their appropriate test class. Leave a simple test in place on the simple regression test to make sure that it behaves properly.

--- tests/SimpleRegressionTest.php
+++ tests/SimpleRegressionTest.php
@@ -4,28 +4,28 @@ use mcordingley\Regression\SimpleRegression;
 
 class SimpleRegressionTest extends PHPUnit_Framework_TestCase
 {
-    protected $simpleSkinnyRegression;
-
-    public function __construct($name = null, array $data = array(), $dataName = '') {
+    protected $simple;
+    
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
         parent::__construct($name, $data, $dataName);
         
-        $this->simpleSkinnyRegression = new SimpleRegression;
-        
-        $this->simpleSkinnyRegression->addData(1, [ 1 ]);
-        $this->simpleSkinnyRegression->addData(2, [ 2 ]);
-        $this->simpleSkinnyRegression->addData(3, [ 1.3 ]);
-        $this->simpleSkinnyRegression->addData(4, [ 3.75 ]);
-        $this->simpleSkinnyRegression->addData(5, [ 2.25 ]);
+        $this->simple = new Simpleregression;
+
+        $this->simple->addData(1, [1]);
+        $this->simple->addData(2, [2]);
+        $this->simple->addData(3, [1.3]);
+        $this->simple->addData(4, [3.75]);
+        $this->simple->addData(5, [2.25]);
     }
     
-    public function testSimpleSkinnyIntercept()
+    public function testCoefficients()
     {
-        $this->assertEquals(1.095497063, round($this->simpleSkinnyRegression->getIntercept(), 9));
+        $this->assertEquals(0.924515989, round($this->simple->getCoefficients()[0], 9));
     }
     
-    public function testSimpleSkinnyCoefficients()
+    public function testIntercept()
     {
-        $coefficients = $this->simpleSkinnyRegression->getCoefficients();
-        $this->assertEquals(0.924515989, round($coefficients[0], 9));
+        $this->assertEquals(1.095497063, round($this->simple->getIntercept(), 9));
     }
 }
