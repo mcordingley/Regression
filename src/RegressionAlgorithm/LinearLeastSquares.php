937a907904e538cddba600de4db9e9434e341787
Michael Cordingley (mcordingley) <Michael.Cordingley@gmail.com>
2015-03-03 23:01:45 -0500

    Refuse regression without enough observations.

--- src/RegressionAlgorithm/LinearLeastSquares.php
+++ src/RegressionAlgorithm/LinearLeastSquares.php
@@ -2,6 +2,7 @@
 
 namespace mcordingley\Regression\RegressionAlgorithm;
 
+use InvalidArgumentException;
 use mcordingley\LinearAlgebra\Matrix;
 
 class LinearLeastSquares implements RegressionAlgorithmInterface
@@ -11,26 +12,16 @@ class LinearLeastSquares implements RegressionAlgorithmInterface
         $design = new Matrix($independentData);
         $observed = (new Matrix([ $dependentData ]))->transpose();
         
-        // Use different math depending on the data dimensions:
-        // http://math.stackexchange.com/questions/381600/singular-matrix-problem
-        
-        if ($design->columns == $design->rows) { // Square
-            $prediction = $design->inverse()->multiply($observed);
-        } elseif ($design->columns > $design->rows) { // Fat
-            $designTranspose = $design->transpose();
-            
-            $prediction = $designTranspose->multiply(
-                $design->multiply($designTranspose)
-                       ->inverse()
-            )->multiply($observed);
-        } else { // Skinny
-            $designTranspose = $design->transpose();
-            
-            $prediction = $designTranspose
-                                 ->multiply($design)
-                                 ->inverse()
-                                 ->multiply($designTranspose->multiply($observed));
+        if ($design->columns >= $design->rows) {
+            throw new InvalidArgumentException('Not enough observations to perform regression. You need to have more observations than explanatory variables.');
         }
+        
+        $designTranspose = $design->transpose();
+
+        $prediction = $designTranspose
+                             ->multiply($design)
+                             ->inverse()
+                             ->multiply($designTranspose->multiply($observed));
         
         // Extract the vertical vector as a simple array.
         return $prediction->transpose()->toArray()[0];
