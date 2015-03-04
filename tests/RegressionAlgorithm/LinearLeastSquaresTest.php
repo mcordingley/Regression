73ee6d3d5804a8c037b7f8959177e68227bcf071
Michael Cordingley (mcordingley) <Michael.Cordingley@gmail.com>
2015-03-03 23:02:14 -0500

    Remove tests that targeted removed code paths in LinearLeastSquares.

--- tests/RegressionAlgorithm/LinearLeastSquaresTest.php
+++ tests/RegressionAlgorithm/LinearLeastSquaresTest.php
@@ -14,12 +14,7 @@ class LinearLeastSquaresTest extends PHPUnit_Framework_TestCase
         $this->strategy = new LinearLeastSquares;
     }
     
-    public function testFatMatrix()
-    {
-        // TODO
-    }
-    
-    public function testSkinnyMatrix()
+    public function testRegress()
     {
         $coefficients = $this->strategy->regress([1, 2, 3, 4, 5], [
             [1, 1],
@@ -31,22 +26,5 @@ class LinearLeastSquaresTest extends PHPUnit_Framework_TestCase
         
         $this->assertEquals(1.095497063, round($coefficients[0], 9));
         $this->assertEquals(0.924515989, round($coefficients[1], 9));
-    }
-    
-    public function testSquareMatrix()
-    {
-        $coefficients = $this->strategy->regress([2, 4, 6, 8, 10], [
-            [1, 3, 5, 7, 2],
-            [1, 3, 2, 1, 5],
-            [1, 1, 2, 3, 4],
-            [1, 1, 3, 4, 7],
-            [1, 19, 17, 15, 14],
-        ]);
-        
-        $this->assertEquals(-2.667, round($coefficients[0], 3));
-        $this->assertEquals(3.333, round($coefficients[1], 3));
-        $this->assertEquals(-9.333, round($coefficients[2], 3));
-        $this->assertEquals(5.333, round($coefficients[3], 3));
-        $this->assertEquals(2, round($coefficients[4], 3));
     }
 }
