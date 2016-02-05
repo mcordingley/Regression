c6a559e39b49c11d225259f950513688b268489b
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-02-05 13:00:25 -0500

    Rename RegressionAlgorithm namespace.

---
+++ src/RegressionAlgorithms/LinearLeastSquares.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace mcordingley\Regression\RegressionAlgorithms;
+
+use InvalidArgumentException;
+use mcordingley\LinearAlgebra\Matrix;
+use mcordingley\Regression\CoefficientSet;
+use mcordingley\Regression\DataBag;
+use mcordingley\Regression\RegressionAlgorithm;
+
+final class LinearLeastSquares implements RegressionAlgorithm
+{
+    public function regress(DataBag $data): CoefficientSet
+    {
+        $dependentData = $data->getDependents();
+        $independentData = $data->getIndependents();
+
+        // Prepend dummies.
+        $independentData = array_map(function ($row) {
+            return array_merge([1], $row);
+        }, $independentData);
+
+        $design = new Matrix($independentData);
+        $observed = (new Matrix([$dependentData]))->transpose();
+
+        if ($design->columns >= $design->rows) {
+            throw new InvalidArgumentException('Not enough observations to perform regression. You need to have more observations than explanatory variables.');
+        }
+
+        $designTranspose = $design->transpose();
+
+        $prediction = $designTranspose
+                             ->multiply($design)
+                             ->inverse()
+                             ->multiply($designTranspose->multiply($observed));
+
+        // Extract the vertical vector as a simple array.
+        return new CoefficientSet($prediction->transpose()->toArray()[0]);
+    }
+}
