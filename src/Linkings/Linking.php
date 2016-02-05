07c11f13f7faa0c1c3247d9866e6532f47cc7bf9
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-02-05 13:51:00 -0500

    Formatting.

--- src/Linkings/Linking.php
+++ src/Linkings/Linking.php
@@ -8,11 +8,11 @@ use mcordingley\Regression\OutputTransformer;
 
 /**
  * Linking
- * 
+ *
  * Inheriting classes are instances of linking functions as defined here:
- * 
+ *
  * https://en.wikipedia.org/wiki/Generalized_linear_model#Link_function
- * 
+ *
  * Each Linking object is a matched pair of the link and mean functions. Only
  * the mean function is used directly by this library. The link function is
  * provided for completeness and convenience.
@@ -21,19 +21,19 @@ abstract class Linking implements OutputTransformer
 {
     /**
      * delinearize
-     * 
+     *
      * Also known as the "Mean Function".
-     * 
+     *
      * @param float $value
      * @return float
      */
     abstract public function delinearize(float $value): float;
-    
+
     /**
      * linearize
-     * 
+     *
      * Also known as the "Link Function".
-     * 
+     *
      * @param float $value
      * @return float
      */
