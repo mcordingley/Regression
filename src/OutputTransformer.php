1e99545cd7f78ac055157e6fb8ef0b705586948b
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-02-06 15:32:50 -0500

    Junk the comment on this interface.

--- src/OutputTransformer.php
+++ src/OutputTransformer.php
@@ -6,13 +6,5 @@ namespace mcordingley\Regression;
 
 interface OutputTransformer
 {
-    /**
-     * delinearize
-     *
-     * Converts the incoming value out of linear space suitable for interpretation.
-     *
-     * @param float $data
-     * @return float
-     */
     public function delinearize(float $data): float;
 }
