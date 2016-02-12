54eae7b5dd3c7ef90b9f561c060358a22c81b8c6
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-02-12 18:37:01 -0500

    Reinstate the Identity linking.

---
+++ src/Linkings/Identity.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace mcordingley\Regression\Linkings;
+
+final class Identity extends Linking
+{
+    public function delinearize(float $value): float
+    {
+        return $value;
+    }
+
+    public function linearize(float $value): float
+    {
+        return $value;
+    }
+
+    public function loss(array $coefficients, array $observations, float $outcome, int $index): float
+    {
+        $sumProduct = Helpers::sumProduct($coefficients->toArray(), $observations);
+        $hypothesis = $this->delinearize($sumProduct);
+
+        return -2 * ($outcome - $hypothesis) * $observations[$i];
+    }
+}
