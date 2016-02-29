3c9482c657defecc1ab785c8461ffdcb7846129a
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-02-29 00:29:29 -0500

    Remove attempts to force corrections on the gradient.

--- src/Linkings/Logistic.php
+++ src/Linkings/Logistic.php
@@ -26,13 +26,7 @@ final class Logistic extends Linking
     public function loss(array $coefficients, array $observations, float $outcome, int $index): float
     {
         $hypothesis = $this->delinearize(Helpers::sumProduct($coefficients, $observations));
-        $error = $outcome - $hypothesis;
 
-        // 0.5 will maximize the gradient, since we're on the wrong side.
-        if ($error < -0.5 || $error > 0.5) {
-            $hypothesis = 0.5;
-        }
-
-        return -2 * $error * $hypothesis * (1.0 - $hypothesis) * $observations[$index];
+        return -2.0 * ($outcome - $hypothesis) * $hypothesis * (1.0 - $hypothesis) * $observations[$index];
     }
 }
