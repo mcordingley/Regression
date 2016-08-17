97d3fce4f03549bb5f40f40b2cf5d84d4e0fb1ca
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-17 16:05:11 -0400

    Rewrite the logistic regression test to be more focused.

--- tests/LogisticTest.php
+++ tests/LogisticTest.php
@@ -6,17 +6,38 @@ use mcordingley\Regression\Algorithm\GradientDescent\Batch;
 use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Fixed;
 use mcordingley\Regression\Algorithm\GradientDescent\Gradient\Logistic as LogisticGradient;
 use mcordingley\Regression\Observations;
-use mcordingley\Regression\Predictor\Logistic as LogisticPredictor;
 use PHPUnit_Framework_TestCase;
 
-final class LogisticTest extends PHPUnit_Framework_TestCase
+/**
+ * Puts the pieces together to show that a Logistic regression will converge. Note that the GRE feature has been
+ * normalized. This leads to convergence in a matter of minutes, instead of hours/never.
+ *
+ * @package mcordingley\Regression\Tests
+ */
+class LogisticTest extends PHPUnit_Framework_TestCase
 {
-    private $predictor;
-
-    public function __construct($name = null, array $data = array(), $dataName = '')
+    /**
+     * @large
+     */
+    public function testRegression()
     {
-        parent::__construct($name, $data, $dataName);
+        $observations = $this->getLogisticObservations();
+        $regression = new Batch(new LogisticGradient, new Fixed(0.125));
 
+        // Example debug line for tuning the descent parameters.
+        //$regression->addDescentIterationListener(new DescentLogger($gradient, $observations));
+
+        static::assertEquals(
+            [-3.98997907333, 0.2264425786179, 0.80403754928, -0.67544292796369, -1.340203916468, -1.5514636769182],
+            $regression->regress($observations)
+        );
+    }
+
+    /**
+     * @return Observations
+     */
+    private function getLogisticObservations()
+    {
         // Data from http://statistics.ats.ucla.edu/stat/r/dae/logit.htm
         $observations = new Observations;
 
@@ -24,29 +45,20 @@ final class LogisticTest extends PHPUnit_Framework_TestCase
         fgetcsv($csv); // Throw away headers.
 
         while ($line = fgetcsv($csv)) {
+            // Split composite feature, since the school rank isn't actually an interval value.
             $rank2 = $line[3] == 2 ? 1 : 0;
             $rank3 = $line[3] == 3 ? 1 : 0;
             $rank4 = $line[3] == 4 ? 1 : 0;
 
-            //                 [1, GRE,            GPA,      Rank2,  Rank3,  Rank4],  Admitted
-            $observations->add([1, $line[1] / 100, $line[2], $rank2, $rank3, $rank4], (float) $line[0]);
+            // Normalize the GRE score. This is critical to get convergence.
+            $gre = $line[1] / 100;
+
+            //                 [1, GRE,  GPA,      Rank2,  Rank3,  Rank4],  Admitted
+            $observations->add([1, $gre, $line[2], $rank2, $rank3, $rank4], (float) $line[0]);
         }
 
         fclose($csv);
 
-        $gradient = new LogisticGradient;
-        $regression = new Batch($gradient, new Fixed(0.125));
-
-        // Example debug line for tuning the descent parameters.
-        //$regression->addDescentIterationListener(new DescentLogger($gradient, $observations));
-
-        $coefficients = $regression->regress($observations);
-
-        $this->predictor = new LogisticPredictor($coefficients);
-    }
-
-    public function testPredict()
-    {
-        $this->assertEquals(0.308, round($this->predictor->predict([1, 2, 3.39, 0, 0, 0]), 3));
+        return $observations;
     }
 }
