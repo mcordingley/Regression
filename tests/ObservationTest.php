4a23f729df2ae4275458f3b2ee13bc3e69739d71
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-17 14:18:00 -0400

    Make a test case for the Observation class.

---
+++ tests/ObservationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace mcordingley\Regression\Tests;
+
+use mcordingley\Regression\Observation;
+use PHPUnit_Framework_TestCase;
+
+class ObservationTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetters()
+    {
+        $observation = new Observation([1, 2], 3);
+
+        static::assertEquals([1, 2], $observation->getFeatures());
+        static::assertEquals(3, $observation->getOutcome());
+    }
+}
