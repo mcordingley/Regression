848302c8cb153224b3066042d9bf6a2813c0e2d5
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-17 15:14:43 -0400

    Test the Adagrad gradient schedule.

---
+++ tests/Algorithm/GradientDescent/Schedule/AdagradTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace mcordingley\Regression\Tests\Algorithm\GradientDescent\Schedule;
+
+use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Adagrad;
+use PHPUnit_Framework_TestCase;
+
+class AdagradTest extends PHPUnit_Framework_TestCase
+{
+    public function testStep()
+    {
+        $schedule = new Adagrad(0.01, 0.000001);
+        $schedule->update([1.0]);
+        static::assertEquals(0.0099999900000100009, $schedule->step(0));
+    }
+}
