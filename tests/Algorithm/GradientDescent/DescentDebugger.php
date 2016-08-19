f6ec36efd1fc0c8769c45cc9c126bdc920f5f2cd
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 17:28:36 -0400

    Add helper trait for descent tests.

---
+++ tests/Algorithm/GradientDescent/DescentDebugger.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace mcordingley\Regression\Tests\Algorithm\GradientDescent;
+
+use mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria\DescentSpy;
+use mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria\StoppingCriteria;
+
+/**
+ * Helper trait for tests to output the results of each gradient descent iteration.
+ *
+ * @package mcordingley\Regression\Tests\Algorithm\GradientDescent
+ */
+trait DescentDebugger
+{
+    private function getDescentSpy(StoppingCriteria $criteria)
+    {
+        $onIteration = function (array $gradient, array $coefficients) {
+            $output = '[[' . implode(',', $gradient) . '], [' . implode(',', $coefficients) . ']]' . "\n";
+            fwrite(STDERR, $output);
+        };
+
+        return new DescentSpy($criteria, $onIteration);
+    }
+}
