b86c65b720cd71beb1a5892eb3f4094bf5275854
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 17:59:28 -0400

    Make tests for the different descent algorithms.

---
+++ tests/Algorithm/GradientDescent/BatchTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace mcordingley\Regression\Tests\Algorithm\GradientDescent;
+
+use mcordingley\Regression\Algorithm\GradientDescent\Batch;
+use mcordingley\Regression\Algorithm\GradientDescent\Gradient\Linear;
+use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Adagrad;
+use mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria\GradientNorm;
+use mcordingley\Regression\Observations;
+use PHPUnit_Framework_TestCase;
+
+class BatchTest extends PHPUnit_Framework_TestCase
+{
+    use DescentDebugger;
+
+    private static $features = [
+        [1, 1],
+        [1, 2],
+        [1, 1.3],
+        [1, 3.75],
+        [1, 2.25],
+    ];
+
+    private static $outcomes = [
+        1,
+        2,
+        3,
+        4,
+        5,
+    ];
+
+    /**
+     * @large
+     */
+    public function testRegression()
+    {
+        $regression = new Batch(new Linear, new Adagrad, new GradientNorm);
+        $coefficients = $regression->regress(Observations::fromArray(static::$features, static::$outcomes));
+
+        $this->assertEquals(1.095, round($coefficients[0], 3));
+        $this->assertEquals(0.925, round($coefficients[1], 3));
+    }
+}
---
+++ tests/Algorithm/GradientDescent/MiniBatchTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace mcordingley\Regression\Tests\Algorithm\GradientDescent;
+
+use mcordingley\Regression\Algorithm\GradientDescent\Gradient\Linear;
+use mcordingley\Regression\Algorithm\GradientDescent\MiniBatch;
+use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Adagrad;
+use mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria\GradientNorm;
+use mcordingley\Regression\Observations;
+use PHPUnit_Framework_TestCase;
+
+class MiniBatchTest extends PHPUnit_Framework_TestCase
+{
+    private static $features = [
+        [1, 1],
+        [1, 2],
+        [1, 1.3],
+        [1, 3.75],
+        [1, 2.25],
+    ];
+
+    private static $outcomes = [
+        1,
+        2,
+        3,
+        4,
+        5,
+    ];
+
+    /**
+     * @large
+     */
+    public function testRegression()
+    {
+        $regression = new MiniBatch(new Linear, new Adagrad, new GradientNorm, 3);
+        $coefficients = $regression->regress(Observations::fromArray(static::$features, static::$outcomes));
+
+        $this->assertEquals(1.095, round($coefficients[0], 3));
+        $this->assertEquals(0.925, round($coefficients[1], 3));
+    }
+}
