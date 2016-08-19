21027751fea486a4ea8bbba81494376282e93fb0
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 17:59:02 -0400

    Mark test as large.

--- tests/LogisticTest.php
+++ tests/LogisticTest.php
@@ -12,11 +12,12 @@ use PHPUnit_Framework_TestCase;
 /**
  * Puts the pieces together to show that a Logistic regression will converge. Note that the GRE feature has been
  * normalized. This leads to convergence in a matter of minutes, instead of hours/never.
- *
- * @package mcordingley\Regression\Tests
  */
 class LogisticTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @large
+     */
     public function testRegression()
     {
         $regression = new Batch(new LogisticGradient, new Fixed(0.125), new GradientNorm);
