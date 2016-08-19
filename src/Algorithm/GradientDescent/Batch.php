a5d057b47e45d5404fc1cfa5d94ce770e377cc87
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 15:38:02 -0400

    Normalize form of calculation to more closely align with other implementations.

--- src/Algorithm/GradientDescent/Batch.php
+++ src/Algorithm/GradientDescent/Batch.php
@@ -15,13 +15,14 @@ final class Batch extends GradientDescent
     protected function calculateGradient(Observations $observations, array $coefficients)
     {
         $gradient = array_fill(0, count($observations->getObservation(0)->getFeatures()), 0.0);
+        $batchSize = count($observations);
 
         /** @var Observation $observation */
         foreach ($observations as $observation) {
             $observationGradient = $this->gradient->gradient($coefficients, $observation->getFeatures(), $observation->getOutcome());
 
             foreach ($observationGradient as $i => $observationSlope) {
-                $gradient[$i] += $observationSlope / count($observations);
+                $gradient[$i] += $observationSlope / $batchSize;
             }
         }
 
