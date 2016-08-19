74780e8fbb04ba073474b07782d5698407f57182
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 15:28:44 -0400

    Add RMSProp as a schedule.

---
+++ src/Algorithm/GradientDescent/Schedule/RmsProp.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace mcordingley\Regression\Algorithm\GradientDescent\Schedule;
+
+class RmsProp implements Schedule
+{
+    /** @var float */
+    private $eta;
+
+    /** @var array */
+    private $history = [];
+
+    /** @var float */
+    private $rate;
+
+    /** @var float */
+    private $stepSize;
+
+    /**
+     * @param float $rate
+     * @param float $stepSize
+     * @param float $eta
+     */
+    public function __construct($rate = 0.9, $stepSize = 0.01, $eta = 0.000001)
+    {
+        $this->stepSize = $stepSize;
+        $this->rate = $rate;
+        $this->eta = $eta;
+    }
+
+    /**
+     * @param array $gradient
+     */
+    public function update(array $gradient)
+    {
+        foreach ($gradient as $i => $slope) {
+            $history = isset($this->history[$i]) ? $this->history[$i] : pow($slope, 2);
+            $this->history[$i] = $history * $this->rate + (1.0  - $this->rate) * pow($slope, 2);
+        }
+    }
+
+    /**
+     * @param int $featureIndex
+     * @return float
+     */
+    public function step($featureIndex)
+    {
+        return $this->stepSize / sqrt($this->eta + $this->history[$featureIndex]);
+    }
+}
---
+++ tests/Algorithm/GradientDescent/Schedule/RmsPropTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace mcordingley\Regression\Tests\Algorithm\GradientDescent\Schedule;
+
+use mcordingley\Regression\Algorithm\GradientDescent\Schedule\RmsProp;
+use PHPUnit_Framework_TestCase;
+
+class RmsPropTest extends PHPUnit_Framework_TestCase
+{
+    public function testStep()
+    {
+        $schedule = new RmsProp(0.9, 0.01, 0.000001);
+        $schedule->update([1.0]);
+        static::assertEquals(0.0099999950000037498, $schedule->step(0));
+    }
+}
