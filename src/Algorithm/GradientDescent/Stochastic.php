a97b2e5bc9f17b25cdcc243ac447d370296a3147
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 00:59:23 -0400

    Modify the SGD to sweep through the training set in random order instead of doing random draws.

--- src/Algorithm/GradientDescent/Stochastic.php
+++ src/Algorithm/GradientDescent/Stochastic.php
@@ -2,10 +2,14 @@
 
 namespace mcordingley\Regression\Algorithm\GradientDescent;
 
+use mcordingley\Regression\Observation;
 use mcordingley\Regression\Observations;
 
 final class Stochastic extends GradientDescent
 {
+    /** @var array */
+    private $shuffled;
+
     /**
      * @param Observations $observations
      * @param array $coefficients
@@ -13,7 +17,22 @@ final class Stochastic extends GradientDescent
      */
     protected function calculateGradient(Observations $observations, array $coefficients)
     {
-        $observation = $observations->getObservation(mt_rand(0, count($observations) - 1));
+        $observation = $this->getObservation($observations);
+
         return $this->gradient->gradient($coefficients, $observation->getFeatures(), $observation->getOutcome());
+    }
+
+    /**
+     * @param Observations $observations
+     * @return Observation
+     */
+    private function getObservation(Observations $observations)
+    {
+        if (!$this->shuffled) {
+            $this->shuffled = range(0, count($observations) - 1);
+            shuffle($this->shuffled);
+        }
+
+        return $observations->getObservation(array_pop($this->shuffled));
     }
 }
