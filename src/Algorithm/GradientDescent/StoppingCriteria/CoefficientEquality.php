7eb4c4cdf9cfb2128577e4f6ec5fa1e2452d810f
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 23:16:24 -0400

    Document the coefficient equality convergence test.

--- src/Algorithm/GradientDescent/StoppingCriteria/CoefficientEquality.php
+++ src/Algorithm/GradientDescent/StoppingCriteria/CoefficientEquality.php
@@ -2,6 +2,14 @@
 
 namespace mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria;
 
+/**
+ * Checks for when the coefficients themselves have completely stopped changing.
+ * This represents a complete convergence of the descent, but can take a long
+ * time to occur, as the update for each weight must become smaller than can be
+ * represented in floating points.
+ *
+ * @package mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria
+ */
 final class CoefficientEquality implements StoppingCriteria
 {
     private $oldCoefficients;
