b6918fea7e4400bd126476efd9aa32faf1a35d52
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 23:31:57 -0400

    Document the Fixed update schedule.

--- src/Algorithm/GradientDescent/Schedule/Fixed.php
+++ src/Algorithm/GradientDescent/Schedule/Fixed.php
@@ -2,6 +2,17 @@
 
 namespace mcordingley\Regression\Algorithm\GradientDescent\Schedule;
 
+/**
+ * Simple step rule that always provides a fixed step size to the descent.
+ * Since the gradient of the error becomes shallower as the descent nears
+ * convergence, this will naturally shrink the updates into the error
+ * function's minimum. However, too large of a step size will lead to the
+ * descent diverging and too small of a step size will lead to an extremely
+ * long descent. Unfortunately, choosing a good step size is a matter of
+ * trial and error.
+ *
+ * @package mcordingley\Regression\Algorithm\GradientDescent\Schedule
+ */
 final class Fixed implements Schedule
 {
     /** @var float */
