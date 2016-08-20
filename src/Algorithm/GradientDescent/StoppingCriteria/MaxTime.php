49c9f4ed1a0559fa2973287826d6d973ddc714ea
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-19 23:25:33 -0400

    Document the MaxTime criteria.

--- src/Algorithm/GradientDescent/StoppingCriteria/MaxTime.php
+++ src/Algorithm/GradientDescent/StoppingCriteria/MaxTime.php
@@ -2,6 +2,14 @@
 
 namespace mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria;
 
+/**
+ * Run the descent for a certain amount of time before stopping. Goes well with
+ * some criteria that checks for convergence within an instance of `Any` to cap
+ * the amount of time that a descent can run. Nice to make sure that a descent
+ * doesn't become runaway on a production server.
+ *
+ * @package mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria
+ */
 final class MaxTime implements StoppingCriteria
 {
     /** @var int */
