0096dbb1f3aa5b35024eaf8eb266b2dacf8cea4a
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-08-22 20:09:26 -0400

    Refactor Stochastic and MiniBatch tests.

--- tests/Algorithm/GradientDescent/MiniBatchTest.php
+++ tests/Algorithm/GradientDescent/MiniBatchTest.php
@@ -2,42 +2,36 @@
 
 namespace mcordingley\Regression\Tests\Algorithm\GradientDescent;
 
-use mcordingley\Regression\Algorithm\GradientDescent\Gradient\Linear;
+use mcordingley\Regression\Algorithm\Algorithm;
+use mcordingley\Regression\Algorithm\GradientDescent\Gradient\Logistic as LogisticGradient;
 use mcordingley\Regression\Algorithm\GradientDescent\MiniBatch;
-use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Adagrad;
+use mcordingley\Regression\Algorithm\GradientDescent\Schedule\Adam;
 use mcordingley\Regression\Algorithm\GradientDescent\StoppingCriteria\GradientNorm;
-use mcordingley\Regression\Observations;
-use PHPUnit_Framework_TestCase;
 
-class MiniBatchTest extends PHPUnit_Framework_TestCase
+class MiniBatchTest extends GradientDescent
 {
-    private static $features = [
-        [1, 1],
-        [1, 2],
-        [1, 1.3],
-        [1, 3.75],
-        [1, 2.25],
-    ];
-
-    private static $outcomes = [
-        1,
-        2,
-        3,
-        4,
-        5,
-    ];
-
     /**
-     * @large
+     * @return Algorithm
      */
-    public function testRegression()
+    protected function makeRegression()
     {
         static::markTestSkipped('Yet to find a good convergence criteria for this type of regression.');
 
-        $regression = new MiniBatch(new Linear, new Adagrad, new GradientNorm, 3);
-        $coefficients = $regression->regress(Observations::fromArray(static::$features, static::$outcomes));
+        return new MiniBatch(new LogisticGradient, new Adam, new GradientNorm, 3);
+    }
 
-        $this->assertEquals(1.095, round($coefficients[0], 3));
-        $this->assertEquals(0.925, round($coefficients[1], 3));
+    /**
+     * @return array
+     */
+    protected function getExpectedCoefficients()
+    {
+        return [
+            -3.9572690927850793,
+            0.22579298444865589,
+            0.79626535291848777,
+            -0.67784339995776333,
+            -1.3416834110939926,
+            -1.55412650298527
+        ];
     }
 }
